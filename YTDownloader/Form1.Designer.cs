950deb286dc879276490d8c91c9d5a9ce6c9ca22
Maze <Maze@WorkPC>
2021-03-24 18:32:34 +0300

    an working example

--- YTDownloader/Form1.Designer.cs
+++ YTDownloader/Form1.Designer.cs
@@ -29,13 +29,96 @@ namespace YTDownloader
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(426, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Fetch";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(23, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(397, 23);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(426, 164);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 41);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Download Selected";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(23, 52);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(397, 400);
+            this.checkedListBox1.TabIndex = 6;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(426, 70);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(128, 41);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Select All";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(426, 117);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(128, 41);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Unselect All";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(566, 470);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "YTDownloader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- YTDownloader/Form1.cs
+++ YTDownloader/Form1.cs
@@ -1,21 +1,114 @@
-﻿using System;
+﻿using NAudio.Lame;
+using NAudio.Wave;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.IO;
 using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using YoutubeExplode;
 
 namespace YTDownloader
 {
     public partial class Form1 : Form
     {
+        private static string playListIdentifier = "list=";
+        private static YoutubeClient youtube = new YoutubeClient();
+        private static List<Tuple<string, string>> mDownloadList = new List<Tuple<string, string>>();
+        private static Queue<string> downloader_queue = new Queue<string>();
+
+        private async Task GetList(string listID)
+        {
+            var playlist = await youtube.Playlists.GetAsync(listID);
+            var title = playlist.Title;
+
+            await foreach (var video in youtube.Playlists.GetVideosAsync(playlist.Id))
+            {
+                var videoTitle = video.Title;
+
+                checkedListBox1.Items.Add($"{videoTitle}");
+                mDownloadList.Add(new Tuple<string, string>(videoTitle, video.Id));
+            }
+        }
+        private async Task GetVideo( string videoID )
+        {
+            var video = await youtube.Videos.GetAsync(videoID);
+            checkedListBox1.Items.Add($"{video.Title}");
+            mDownloadList.Add(new Tuple<string, string>(video.Title, video.Id));
+        }
+
+        private bool DownloadAndSave( string title )
+        {
+            try
+            {
+                var videoID = mDownloadList.Find(x => x.Item1 == title)?.Item2;
+                if (String.IsNullOrEmpty(videoID)) return false;
+
+                var streamManifest = youtube.Videos.Streams.GetManifestAsync(videoID).GetAwaiter().GetResult();
+                var streamInfo = streamManifest.GetAudioOnly().FirstOrDefault();
+                var kekwaitstream = youtube.Videos.Streams.GetAsync(streamInfo).GetAwaiter().GetResult();
+
+                var filename = string.Join("_", title.Split(Path.GetInvalidFileNameChars()));
+
+                using (var reader = new AudioFileReader(streamInfo.Url))
+                using (var writer = new LameMP3FileWriter($"{AppDomain.CurrentDomain.BaseDirectory}Output\\{filename}.mp3", reader.WaveFormat, 128))
+                    reader.CopyTo(writer);
+
+                return true;
+            }
+            catch
+            {
+                Thread.Sleep(300);
+                return DownloadAndSave(title);
+            }
+        }
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            string yturi = textBox1.Text;
+            var isPlayList = yturi.Contains(playListIdentifier);
+
+            if (isPlayList)
+                await GetList(yturi);
+            else
+                await GetVideo(yturi);
+        }
+        
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists($"{AppDomain.CurrentDomain.BaseDirectory}Output")) Directory.CreateDirectory($"{AppDomain.CurrentDomain.BaseDirectory}Output");
+
+            foreach (var video_title in checkedListBox1.CheckedItems)
+                downloader_queue.Enqueue(video_title.ToString());
+
+            //10 Thread Downloader
+            for (int i = 0; i < 10; i++)
+            {
+                new Thread(new ThreadStart(() => {
+                    while (downloader_queue.Count > 0)
+                    {
+                        DownloadAndSave(downloader_queue.Dequeue());
+                    }
+                })).Start();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            for (var i = 0; i < checkedListBox1.Items.Count; i++)
+                checkedListBox1.SetItemChecked(i, true);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            for (; checkedListBox1.CheckedIndices.Count > 0;)
+                checkedListBox1.SetItemChecked(checkedListBox1.CheckedIndices[0], false);
+        }
     }
 }
